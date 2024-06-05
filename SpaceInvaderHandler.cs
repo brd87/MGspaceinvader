96f42c0d41217a755e94859c20c3b706a6c2c659
brd <hubertx1804@gmail.com>
2024-06-05 10:35:29 +0200

    wsad

--- SpaceInvaderHandler.cs
+++ SpaceInvaderHandler.cs
@@ -12,11 +12,11 @@ namespace SpaceInvaderPlusPlus
     {
         public General _general;
         private GraphicsDeviceManager _graphics;
-        private SpaceBackground spaceBackground;
-        private MainWorld world;
-        private MainMenu menu;
-        private string music_menu;
-        private string music_combat;
+        private SpaceBackground _spaceBackground;
+        private MainWorld _world;
+        private MainMenu _menu;
+        private string _music_menu;
+        private string _music_combat;
 
         public SpaceInvaderHandler()
         {
@@ -45,17 +45,17 @@ namespace SpaceInvaderPlusPlus
             _general.SPRITE_BATCH = new SpriteBatch(GraphicsDevice);
             _general.ASSETLIBRARY = new AssetLibrary(ref _general);
 
-            spaceBackground = new SpaceBackground(ref _general);
-            world = new MainWorld(ref _general);
-            menu = new MainMenu(Window, ref _general);
+            _spaceBackground = new SpaceBackground(ref _general);
+            _world = new MainWorld(ref _general);
+            _menu = new MainMenu(Window, ref _general);
 
             _graphics.IsFullScreen = false;
             _graphics.PreferredBackBufferWidth = _general.WIDTH;
             _graphics.PreferredBackBufferHeight = _general.HEIGHT;
             _graphics.ApplyChanges();
 
-            music_menu = "music/menu";
-            music_combat = "music/combat";
+            _music_menu = "music/menu";
+            _music_combat = "music/combat";
             MediaPlayer.Play(_general.ASSETLIBRARY.bg_music_menu);
             MediaPlayer.IsRepeating = true;
             MediaPlayer.Volume = _general.SETTINGS.LastMusicVolume;
@@ -65,29 +65,29 @@ namespace SpaceInvaderPlusPlus
         {
             _general.KSTATE = Keyboard.GetState();
 
-            Thread BgThread = new Thread(x => spaceBackground.Update(ref _general));
+            Thread BgThread = new Thread(x => _spaceBackground.Update(ref _general));
             BgThread.Start();
 
             if (_general.GAMESTATE == 0)
             {
-                if ("music/" + MediaPlayer.Queue.ActiveSong.Name != music_menu)
+                if ("music/" + MediaPlayer.Queue.ActiveSong.Name != _music_menu)
                     MediaPlayer.Play(_general.ASSETLIBRARY.bg_music_menu);
-                menu.Update(ref gameTime, ref _general);
+                _menu.Update(ref gameTime, ref _general);
             }
             else if (_general.GAMESTATE == 1)
             {
-                if ("music/" + MediaPlayer.Queue.ActiveSong.Name != music_combat)
+                if ("music/" + MediaPlayer.Queue.ActiveSong.Name != _music_combat)
                     MediaPlayer.Play(_general.ASSETLIBRARY.bg_music_combat);
-                world.RanNew(ref _general);
+                _world.RanNew(ref _general);
                 _general.GAMESTATE = 2;
             }
             else if (_general.GAMESTATE == 2)
             {
-                world.Update(ref gameTime, ref _general);
+                _world.Update(ref gameTime, ref _general);
             }
             else if (_general.GAMESTATE == 3)
             {
-                world.CleanUp();
+                _world.CleanUp();
                 _general.GAMESTATE = 0;
             }
             else
@@ -107,13 +107,13 @@ namespace SpaceInvaderPlusPlus
 
             _general.SPRITE_BATCH.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend);
 
-            Thread BgThread = new Thread(x => spaceBackground.DrawAll(ref _general));
+            Thread BgThread = new Thread(x => _spaceBackground.DrawAll(ref _general));
             BgThread.Start();
 
             if (_general.GAMESTATE == 0)
-                menu.Draw(ref _general);
+                _menu.Draw(ref _general);
             else if (_general.GAMESTATE == 2)
-                world.Draw(ref _general);
+                _world.Draw(ref _general);
 
             BgThread.Join();
 
